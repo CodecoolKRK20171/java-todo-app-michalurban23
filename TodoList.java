2b93d61f676c58a82998e46782c71f609b00e807
Michał Urban <michalurban23@gmail.com>
2017-08-25 12:54:47 +0200

    Add TodoList with basic frame

--- TodoList.java
+++ TodoList.java
@@ -2,6 +2,28 @@
  * Class representing a todo list.
  * TodoList object should aggregate TodoItem objects.
  */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class TodoList{
+
+    private ArrayList<TodoItem> listOfTasks;
+    private String name;
+
+    public TodoList(String name) {
+        this.name = name;
+        this.listOfTasks = new ArrayList<TodoItem>();
+    }
+
+    public void printAll() {
+
+        int index = 1;
+        for (TodoItem item : listOfTasks) {
+            String mark = item.getCompleted() ? " " : "x";
+            String status = "[" + mark + "]";
+            System.out.println(index++ + ") " + status + item);
+        }
+    }
+    // list; mark; unmark; create; add;
 
 }
