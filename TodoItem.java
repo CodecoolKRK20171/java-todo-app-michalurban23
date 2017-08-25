6af569b56ac3b4c8914c7aaad0a2aa9159e3eb18
Michał Urban <michalurban23@gmail.com>
2017-08-25 12:54:31 +0200

    Add TodoItem.java with constructor and fields

--- TodoItem.java
+++ TodoItem.java
@@ -1,7 +1,30 @@
 /**
  * Class representing a single todo item.
- * 
+ *
  */
 public class TodoItem{
 
+    private String name;
+    private Boolean completed;
+
+    public TodoItem(String name) {
+        this.name = name;
+        this.completed = false;
+    }
+
+    public Boolean getCompleted() {
+        return this.completed;
+    }
+
+    public void setCompletedTrue() {
+        this.completed = true;
+    }
+
+    public void setCompletedFalse() {
+        this.completed = false;
+    }
+
+    public String toString() {
+        return this.name;
+    }
 }
