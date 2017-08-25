2d67907410e994713406f43c36a69a0c93bb4f7f
Michał Urban <michalurban23@gmail.com>
2017-08-25 13:16:57 +0200

    Add all required funcionalities

--- TodoList.java
+++ TodoList.java
@@ -15,15 +15,55 @@ public class TodoList{
         this.listOfTasks = new ArrayList<TodoItem>();
     }
 
-    public void printAll() {
+    public void listItems() {
 
-        int index = 1;
-        for (TodoItem item : listOfTasks) {
-            String mark = item.getCompleted() ? " " : "x";
-            String status = "[" + mark + "]";
-            System.out.println(index++ + ") " + status + item);
+        if (listOfTasks.isEmpty()) {
+            System.out.println("List is empty");
+        } else {
+            int index = 1;
+            System.out.println("List of tasks:\n");
+            for (TodoItem item : listOfTasks) {
+                String mark = item.getCompleted() ? "x" : " ";
+                String status = "[" + mark + "] ";
+                System.out.println(index++ + ") " + status + item);
+            }
+            System.out.println("");
         }
     }
-    // list; mark; unmark; create; add;
+
+    public void addItem() {
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("What is the name of this new item?");
+        TodoItem newItem = new TodoItem(read.nextLine());
+        listOfTasks.add(newItem);
+    }
+
+    public void markItem() {
+        listItems();
+        System.out.println("Which item do you want to mark?");
+        Scanner read = new Scanner(System.in);
+
+        Integer index = read.nextInt();
+        listOfTasks.get(index-1).setCompletedTrue();
+    }
+
+    public void unmarkItem() {
+        listItems();
+        System.out.println("Which item do you want to unmark?");
+        Scanner read = new Scanner(System.in);
+
+        Integer index = read.nextInt();
+        listOfTasks.get(index-1).setCompletedFalse();
+    }
+
+    public void archiveItems() {
+
+        for (TodoItem item : listOfTasks) {
+            if (item.getCompleted()) {
+                listOfTasks.remove(item);
+            }
+        }
+    }
 
 }
